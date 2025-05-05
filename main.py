b4f7bb33077b4057252ebc467d17566793fa91c9
Nathan <nathan.watson.172@gmail.com>
2025-05-05 20:14:05 +0100

    Refactored

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import discord
-from discord.ext import commands
-import os
 from dotenv import load_dotenv
-from rich.console import Console
+import os
 
-console = Console()
-
-#* Environment Setup *#
+#* Load from dotenv *#
 load_dotenv()
-temp_token = os.getenv("TOKEN")
-token = temp_token if temp_token != None else ""
+TOKEN = os.getenv("TOKEN")
 
-#* Bot Setup *#
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="?", intents=intents)
 
